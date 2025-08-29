bb04b860c7b0fc666f8a5fb220270cd2c1fcb1b4
RoboRabbit666 <68304655+RoboRabbit666@users.noreply.github.com>
2025-08-29 17:16:12 +0800

    fix bugs

--- app/core/components/components.py
+++ app/core/components/components.py
@@ -3,7 +3,12 @@
 
 from app.config.config import settings
 from czce_ai.embedder import BgeM3Embedder
-from czce_ai.knowledge import DocumentKnowledge, SQLSchemaKnowledge
+from czce_ai.knowledge import (
+    DocumentKnowledge,
+    BusinessInfoKnowledge,
+    QAPairKnowledge,
+    SQLSchemaKnowledge,
+)
 from czce_ai.llm.chat import LLMChat
 from czce_ai.nlp import NLPToolkit
 from czce_ai.reranker import MxbaiReranker
@@ -14,7 +19,6 @@ from resources import (
     USER_DICT_PATH,
     NER_PATTERNs_PATH,
 )
-
 
 tokenizer = NLPToolkit(
     user_dict_path=USER_DICT_PATH,
@@ -27,7 +31,6 @@ embedder = BgeM3Embedder(
 )
 mxbai_reranker = MxbaiReranker(base_url=settings.mxbai_reranker.base_url)
 
-
 # 初始化Document的知识库
 document_kb = DocumentKnowledge(
     tokenizer=tokenizer,
@@ -35,6 +38,7 @@ document_kb = DocumentKnowledge(
     uri=settings.milvus.uri,
     reranker=mxbai_reranker,
 )
+
 # 初始化SQLSchema的知识库
 sql_kb = SQLSchemaKnowledge(
     tokenizer=tokenizer,
@@ -43,28 +47,57 @@ sql_kb = SQLSchemaKnowledge(
     reranker=mxbai_reranker,
 )
 
-# 初始化 qwq_llm
-qwq_llm = LLMChat(
-    name="QwQ",
-    base_url=settings.qwq_llm.base_url,
-    model=settings.qwq_llm.model_name,
-    api_key=settings.qwq_llm.api_key,
-    temperature=settings.qwq_llm.temperature,
-    max_tokens=8192,
+# 初始化QAPair的知识库
+qa_pair_kb = QAPairKnowledge(
+    tokenizer=tokenizer,
+    embedder=embedder,
+    uri=settings.milvus.uri,
+    reranker=mxbai_reranker,
 )
 
-# 初始化 gwen3_11m
+# 初始化Business Info的知识库
+business_info_kb = BusinessInfoKnowledge(
+    tokenizer=tokenizer,
+    embedder=embedder,
+    uri=settings.milvus.uri,
+    reranker=mxbai_reranker,
+)
+
+# 初始化 qwen3_moe_llm
+qwen3_moe_llm = LLMChat(
+    name="Qwen3-30B-A3B",
+    base_url=settings.qwen3_moe_llm.base_url,
+    model=settings.qwen3_moe_llm.model_name,
+    api_key=settings.qwen3_moe_llm.api_key,
+    temperature=settings.qwen3_moe_llm.temperature,
+    top_p=0.8,
+    max_tokens=16384,
+    presence_penalty=1.5,
+)
+
+qwen3_14b_llm = LLMChat(
+    name="Qwen3-14B",
+    base_url=settings.qwen3_14b_llm.base_url,
+    model=settings.qwen3_14b_llm.model_name,
+    api_key=settings.qwen3_14b_llm.api_key,
+    temperature=settings.qwen3_14b_llm.temperature,
+    top_p=0.8,
+    max_tokens=8192,
+    presence_penalty=1.5,
+    extra_body={"chat_template_kwargs": {"enable_thinking": False}},
+)
+# 初始化 qwen3_llm
 qwen3_thinking_llm = LLMChat(
-    name="Owen3-Thinking",
+    name="Qwen3-Thinking",
     base_url=settings.qwen3_llm.base_url,
     model=settings.qwen3_llm.model_name,
     api_key=settings.qwen3_llm.api_key,
     temperature=settings.qwen3_llm.temperature,
-    #top_k=20,
-    top_p=8,
+    # top_k=20,
+    top_p=0.8,
+    max_tokens=8192,
     presence_penalty=1.5,
 )
-
 #不带思考的qwen3
 qwen3_llm = LLMChat(
     name="Qwen3",
@@ -72,7 +105,7 @@ qwen3_llm = LLMChat(
     model=settings.qwen3_llm.model_name,
     api_key=settings.qwen3_llm.api_key,
     temperature=settings.qwen3_llm.temperature,
-    #top_k=20,
+    # top_k=20,
     top_p=0.8,
     max_tokens=8192,
     presence_penalty=1.5,
@@ -87,11 +120,9 @@ minio = MinioClient(
     secure=False,
 )
 
-
-
 def get_llm(thinking: bool, model="Qwen3") -> LLMChat:
-    """“根据是否思考选择不同的LLM"""
-    return qwen3_thinking_llm if thinking else qwen3_llm
+    """根据是否思考选择不同的LLM"""
+    return qwen3_thinking_llm if thinking else qwen3_moe_llm
 
 def get_llm_doc_inspect(thinking: bool, model="Qwen3") -> LLMChat:
     qwen3_llm_di = LLMChat(
@@ -106,5 +137,4 @@ def get_llm_doc_inspect(thinking: bool, model="Qwen3") -> LLMChat:
         frequency_penalty=0.1,
         extra_body={"chat_template_kwargs": {"enable_thinking": False}},
     )
-
     return qwen3_llm_di
